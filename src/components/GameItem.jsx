0a80589f6aa0281868943f8f55c80600c00efc00
fshdq <m.farras@rocketmail.com>
2022-07-27 10:46:40 +0700

    feat: game platform

--- src/components/GameItem.jsx
+++ src/components/GameItem.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const GameItem = ({ gameItem }) => {
   return (
     <>
-      <div key={gameItem.id} className="block overflow-hidden rounded-lg shadow-sm bg-gray-900">
+      <div
+        key={gameItem.id}
+        className="block overflow-hidden rounded-lg shadow-sm bg-gray-900"
+      >
         <img
           className="object-cover w-full h-56"
           src={gameItem.background_image}
@@ -11,9 +14,22 @@ const GameItem = ({ gameItem }) => {
         />
 
         <div className="p-6">
-          <h5 className="text-xl font-bold">
-            {gameItem.name}
-          </h5>
+          <h5 className="text-xl font-bold">{gameItem.name}</h5>
+
+          <div className="grid gap-2 grid-cols-4">
+            {gameItem.platforms?.map((platform) => (
+              <span
+                key={platform.platform.id}
+                className="flex justify-center h-8 bg-black p-2 rounded-full"
+              >
+                <img
+                  src={require(`../assets/${platform.platform.id}.svg`)}
+                  alt={platform.platform.name}
+                  className="rounded"
+                />
+              </span>
+            ))}
+          </div>
 
           <p className="mt-2 text-sm text-gray-500">
             Rating: {gameItem.rating}
